0edd33f6376a866e7c2d6da630352e2084fb8f31
baratynskiy <baratynskiy@gmail.com>
2017-02-10 16:41:42 +0300

    Javac types

---
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/javac/javaToKotlinElements/JavacType.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.cli.jvm.javac.javaToKotlinElements
+
+import org.jetbrains.kotlin.load.java.structure.JavaAnnotation
+import org.jetbrains.kotlin.load.java.structure.JavaAnnotationOwner
+import org.jetbrains.kotlin.load.java.structure.JavaType
+import org.jetbrains.kotlin.name.FqName
+import javax.lang.model.type.TypeKind
+import javax.lang.model.type.TypeMirror
+
+open class JavacType<out T : TypeMirror>(val typeMirror: T) : JavaType, JavaAnnotationOwner {
+
+    companion object {
+        fun <TM : TypeMirror> create(t: TM) = when {
+            t.kind.isPrimitive || t.toString() == "void" -> JavacPrimitiveType(t)
+            t.kind == TypeKind.DECLARED || t.kind == TypeKind.TYPEVAR -> JavacClassifierType(t)
+            t.kind == TypeKind.WILDCARD -> JavacWildcardType(t)
+            t.kind == TypeKind.ARRAY -> JavacArrayType(t)
+            else -> throw UnsupportedOperationException("Unsupported type: $t")
+        }
+    }
+
+    override val annotations: Collection<JavaAnnotation> = emptyList()
+
+    override fun findAnnotation(fqName: FqName) = null
+
+    override val isDeprecatedInJavaDoc = false
+
+}
